ea9fbc28847e8458f4fec7a51d7cabea4406f9f7
Tatiana Dementeva <Gitli@mail.ru>
2019-12-21 03:11:40 +0700

    MM-37 Create POST request for add expense

---
+++ src/core/api-config.js
@@ -0,0 +1,39 @@
+export const fetchTasks = {
+  path: "/api/v1/tasks",
+  method: "GET"
+};
+
+export const createTask = {
+  path: "/api/v1/tasks",
+  method: "POST"
+};
+// my api
+export const fetchCommoditiesByDay = {
+  path: "/api/commodities/day/:date",
+  method: "GET"
+};
+
+export const fetchCommoditiesByWeek = {
+  path: "/api/commodities/week/:date",
+  method: "GET"
+};
+
+export const fetchCommoditiesByMonth = {
+  path: "/api/commodities/month/:date",
+  method: "GET"
+};
+
+export const fetchCommoditiesByYear = {
+  path: "/api/commodities/Year/:date",
+  method: "GET"
+};
+
+export const fetchAllTags = {
+  path: "/api/tags",
+  method: "GET"
+};
+
+export const createCommidity = {
+  path: "/api/expense",
+  method: "POST"
+};
