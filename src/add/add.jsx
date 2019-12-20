62bbbc2499f295a666c8fbac2efa0970979ea92d
Tatiana Dementeva <Gitli@mail.ru>
2019-12-21 00:46:00 +0700

    MM-33 Create field for expense and profit in Add's page

--- src/add/add.jsx
+++ src/add/add.jsx
@@ -6,9 +6,68 @@ class Add extends Component {
     return (
       <div className="wrapper">
         <div className="add-header">
-            <div className="add-header__name">Покупка</div>
-            <div className="add-header__name">Доход</div>
-            
+          <div className="add-header__name">Покупка</div>
+          <div className="add-header__name">Доход</div>
+        </div>
+        <div className="add-expense">
+          <form className="add-commodities" onSubmit={this.onSubmit}>
+            <input
+              className="add-commodities__name"
+              type="text"
+              name="name"
+              placeholder="Дата: дд.мм.гггг"
+              ref={this.dateRef}
+            />
+            <input
+              className="add-commodities__name"
+              type="text"
+              name="name"
+              placeholder="Покупка"
+              ref={this.nameRef}
+            />
+            <input
+              className="add-commodities__price"
+              type="text"
+              name="price"
+              placeholder="Цена"
+              ref={this.priceRef}
+            />
+            <input
+              className="add-commodities__button"
+              type="submit"
+              value="Добавить"
+            />
+          </form>
+        </div>
+        <div className="add-expense">
+          <form className="add-commodities" onSubmit={this.onSubmit}>
+            <input
+              className="add-commodities__name"
+              type="text"
+              name="name"
+              placeholder="Дата: дд.мм.гггг"
+              ref={this.dateRef}
+            />
+            <input
+              className="add-commodities__name"
+              type="text"
+              name="name"
+              placeholder="Покупка"
+              ref={this.nameRef}
+            />
+            <input
+              className="add-commodities__price"
+              type="text"
+              name="price"
+              placeholder="Цена"
+              ref={this.priceRef}
+            />
+            <input
+              className="add-commodities__button"
+              type="submit"
+              value="Добавить"
+            />
+          </form>
         </div>
       </div>
     );
